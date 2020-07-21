7d51758eabd192408679c139264acd3d9ac31d6d
Audrey <audreymedaino@gmail.com>
2020-07-21 12:26:35 -0400

    Exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,32 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+# First time creates a new record and saves it to the database
+
+burnaby = Store.new
+burnaby.name = "Burnaby"
+burnaby.annual_revenue = 300000
+burnaby.mens_apparel = true
+burnaby.womens_apparel = true
+burnaby.save
+
+# Second time can just be created as the table items have already been saved
+richmond = Store.create(
+  name = "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false, 
+  womens_apparel: true
+)
+
+# if a block is provided, both create and new will yield the new object to that block for initialization:
+
+gastown = Store.new do |store|
+  store.name = "Gastown"
+  store.annual_revenue = 190000
+  store.mens_apparel = true
+  store.womens_apparel = false
+  store.save
+end
+
+puts Store.count
+
